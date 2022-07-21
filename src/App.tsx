9f4664084871a822f380ea3f70d4503f904690fb
sandrorochadev <sandrorochadev@gmail.com>
2022-07-21 20:07:11 -0300

    feat: adicionado aplicação completa.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Widget } from "./components/Widget";
+
+export function App() {
+ return (
+ <>
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h1 className="flex justify-center text-9xl font-bold text-zinc-200">FeedWidget</h1>
+    <span className="text-4xl">Feedback Widget</span>
+  </div>
+  <Widget />
+ </>
+ )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
-import './main.css';
+import './global.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
