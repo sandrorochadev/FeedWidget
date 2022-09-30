aa0a044fc9749c296f6e209c35ef5a750a07d86f
sandrorochadev <sandrorochadev@gmail.com>
2022-09-30 18:34:38 -0300

    feat: Adicionado ponto de interrupção no App para texto responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export function App() {
  return (
  <>
   <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="flex justify-center text-9xl font-bold text-zinc-200">FeedWidget</h1>
+    <h1 className="flex justify-center md:text-9xl text-5xl font-bold text-zinc-200">FeedWidget</h1>
     <span className="text-4xl">Feedback Widget</span>
   </div>
   <Widget />
